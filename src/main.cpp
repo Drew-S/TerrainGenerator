f2730b29e28075d4721a025e3cc084bc18432e1c
Drew Sommer <drwsommer@gmail.com>
2020-04-16 10:06:17 -0600

    Added Node editor to window

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,19 @@
 #include <QMainWindow>
 #include <QDebug>
 
+#include <nodeeditor/include/nodes/FlowScene>
+#include <nodeeditor/include/nodes/FlowView>
+#include <nodeeditor/include/nodes/DataModelRegistry>
+// #include <nodes/FlowScene>
+// #include <nodes/FlowView>
+// #include <nodes/DataModelRegistry>
+
+static std::shared_ptr<QtNodes::DataModelRegistry> registerDataModels()
+{
+    std::shared_ptr<QtNodes::DataModelRegistry> registry = std::make_shared<QtNodes::DataModelRegistry>();
+    return registry;
+}
+
 int main(int argc, char *argv[])
 {
     qDebug() << "Starting Program";
@@ -13,6 +26,11 @@ int main(int argc, char *argv[])
     QMainWindow main_window;
 
     ui.setupUi(&main_window);
+
+    QtNodes::FlowScene *scene = new QtNodes::FlowScene(registerDataModels());
+    QtNodes::FlowView *view = new QtNodes::FlowView(scene);
+
+    ui.NodeEditorLayout->addWidget(view);
 
     main_window.show();
 
