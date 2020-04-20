3a9e1813a70f0648e14f07c1d0549dc584362f72
Drew Sommer <drwsommer@gmail.com>
2020-04-20 14:38:04 -0600

    Draw sun point/lines/debug

--- src/OpenGL/Light/light.cpp
+++ src/OpenGL/Light/light.cpp
@@ -1,15 +1,72 @@
 #include "light.h"
 
-#include <QMatrix4x4>
+#include <QOpenGLShader>
+#include <QOpenGLBuffer>
 
-Light::Light(float rotation_x, float rotation_y, float intensity, QColor color)
-    : _rotation_x(rotation_x), _rotation_y(rotation_y), _intensity(intensity), _color(color) {}
+#include <GL/gl.h>
 
-QVector3D Light::direction()
+// TODO: Add control to set light color
+#define LIGHT_COLOR 1.0f, 1.0f, 1.0f
+
+// Get the position of the light in world coordinates
+QVector3D Light::position()
 {
+    // Apply rotational transformations
     QMatrix4x4 matrix;
-    matrix.rotate(this->_rotation_x, QVector3D(1.0f, 0.0f, 0.0f));
     matrix.rotate(this->_rotation_y, QVector3D(0.0f, 1.0f, 0.0f));
+    matrix.rotate(this->_rotation_x, QVector3D(-1.0f, 0.0f, 0.0f));
 
-    return matrix * QVector3D(0.0f, 0.0f, 1.0f);
+    // Apply rotation to a placed light
+    return matrix * QVector3D(0.0f, 0.0f, 2.0f);
+}
+
+// Used to draw the sun into the world
+// TODO: Improve the sun icon appearance
+void Light::paintGL(QMatrix4x4 camera)
+{
+    QVector3D pos = this->position();
+
+    // Draw a point sun
+    glPointSize(5.0f);
+
+    // The origin relative to the camera transformation
+    QVector3D zero = camera * QVector3D();
+
+    // The light location on the y=0 plane
+    QVector3D plane = camera * QVector3D(pos.x(), 0.0f, pos.z());
+
+    // The position of the light relative to the camera transformation
+    pos = camera * pos;
+
+    // TODO: Include a toggle to disable this in the UI
+
+    // Draw the sun point
+    glBegin(GL_POINTS);
+    glColor3f(LIGHT_COLOR);
+    glVertex3f(pos.x(), pos.y(), pos.z());
+    glEnd();
+
+    // Draw lines to display the lights direction to the terrain
+    glPushAttrib(GL_ENABLE_BIT);
+
+    // Draw a dashed lines
+    glLineStipple(4, 0xAAAA);
+    glEnable(GL_LINE_STIPPLE);
+    glBegin(GL_LINES);
+    glColor3f(LIGHT_COLOR);
+
+    // Draw from the sun position to the origin
+    glVertex3f(pos.x(), pos.y(), pos.z());
+    glVertex3f(zero.x(), zero.y(), zero.z());
+
+    // Draw from the sun position on the y-plane to the origin
+    glVertex3f(plane.x(), plane.y(), plane.z());
+    glVertex3f(zero.x(), zero.y(), zero.z());
+
+    // Draw from the sun position to the y-plane
+    glVertex3f(pos.x(), pos.y(), pos.z());
+    glVertex3f(plane.x(), plane.y(), plane.z());
+    glEnd();
+
+    glPopAttrib();
 }
--- src/OpenGL/Light/light.h
+++ src/OpenGL/Light/light.h
@@ -1,32 +1,75 @@
 #pragma once
 
-#include <QColor>
+#include <QVector>
 #include <QVector3D>
+#include <QOpenGLFunctions>
+#include <QOpenGLShaderProgram>
+#include <QMatrix4x4>
 
+// Manages the lights position for use in OpenGL shaders
 class Light
 {
 public:
-    // Light is considered a sun, so no position only direction
     Light(){};
-    Light(float rotation_x, float rotation_y, float intensity, QColor color);
     ~Light(){};
 
-    QVector3D direction();
+    // Returns the position of the light in world space
+    QVector3D position();
 
+    // Returns the intensity of the light
     float intensity() { return this->_intensity; };
 
-    QColor color() { return this->_color; };
+    // Returns the color of the light as QVector3D for easy use in OpenGL shaders
+    QVector3D color() { return this->_color; };
 
+    // Rotates the light around the terrain (turntable)
     void rotateY(float rotation) { this->_rotation_y += rotation; };
-    void rotateX(float rotation) { this->_rotation_x += rotation; };
+
+    // Rotates the light over the terrain
+    void rotateX(float rotation)
+    {
+        this->_rotation_x += rotation;
+        this->_clampRotationX(); // Limits the rotation
+    };
+
+    // Sets the rotation around the terrain (turntable)
     void setRotationY(float rotation) { this->_rotation_y = rotation; };
-    void setRotationX(float rotation) { this->_rotation_x = rotation; };
+
+    // Sets the rotation over the terrain
+    void setRotationX(float rotation)
+    {
+        this->_rotation_x = rotation;
+        this->_clampRotationX(); // Limits the rotation
+    };
+
+    // Set the intensity of the light
     void setIntensity(float intensity) { this->_intensity = intensity; };
-    void setColor(QColor color) { this->_color = color; };
+
+    // Set the color of the light
+    void setColor(QVector3D color) { this->_color = color; };
+
+    // Draw the sun onto the screen
+    void paintGL(QMatrix4x4 camera);
 
 private:
+    // Limits the rotation of the camera over the terrain
+    void _clampRotationX()
+    {
+        if (this->_rotation_x < 0.0)
+            this->_rotation_x = 0.0;
+        if (this->_rotation_x > 90.0)
+            this->_rotation_x = 90.0;
+    };
+
+    // Rotation over the terrain 0 -> 90
     float _rotation_x = 0.0f;
+
+    // Rotation around the terrain (turntable)
     float _rotation_y = 0.0f;
+
+    // Lights intensity
     float _intensity = 1.0f;
-    QColor _color{255, 255, 255, 255};
+
+    // Lights color (defaults to white)
+    QVector3D _color{1.0f, 1.0f, 1.0f};
 };
