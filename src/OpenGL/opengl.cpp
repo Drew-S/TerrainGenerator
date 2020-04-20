53258113cb90b86f3772c1cc6f2d7f25102fbf45
Drew Sommer <drwsommer@gmail.com>
2020-04-20 14:36:53 -0600

    antialias, depth, draw debug axis, input controls

--- src/OpenGL/opengl.cpp
+++ src/OpenGL/opengl.cpp
@@ -2,27 +2,50 @@
 
 #include <QOpenGLFunctions>
 #include <QOpenGLContext>
+#include <QSurfaceFormat>
 #include <QPoint>
 
 #include <GL/gl.h>
 
-// TODO: Added openGL support checks and warning information to user
+// TODO: add ui support for selecting sky color
+#define SKY 0.77f, 0.84f, 0.99f, 1.0f
+
+// TODO: Add openGL support checks and warning information to user
+// Creates an OpenGL widget (QOpenGLWidget)
 OpenGL::OpenGL(QWidget *parent) : QOpenGLWidget(parent)
 {
-    this->_terrain = new Terrain(8);
+    // Set surface format detail
+    QSurfaceFormat fmt;
+
+    // Set antialias samples
+    fmt.setSamples(10);
+
+    // Set depth buffer size
+    fmt.setDepthBufferSize(48);
+
+    // Set format for OpenGL widget to use
+    this->setFormat(fmt);
+
+    // Create a terrain object with a set resolution
+    // TODO: add support for changing terrain resolution
+    // WARNING: Maxed out at around 256^2 vertices
+    this->_terrain = new Terrain(256);
+
+    // Create camera
     this->_camera = new Camera;
+
+    // Create light
     this->_light = new Light;
 
-    // Set default camera location
+    // Set default camera rotation and zoom
     this->_camera->setRotationX(45.0f);
-    this->_camera->setRotationY(45.0f);
-    this->_camera->setZoom(8.0f);
+    this->_camera->setZoom(4.243f);
 
-    // Set default light angle
+    // Set default light rotation
     this->_light->setRotationX(45.0f);
-    this->_light->setRotationY(90.0f);
 }
 
+// Clean up objects
 OpenGL::~OpenGL()
 {
     delete this->_terrain;
@@ -31,7 +54,6 @@ OpenGL::~OpenGL()
 }
 
 // Initialize OpenGL functions and objects
-// TODO: add light
 void OpenGL::initializeGL()
 {
     // Get the open gl functions. f->* is the same as the "raw" opengl functions with f-> prefixed on
@@ -39,9 +61,16 @@ void OpenGL::initializeGL()
     // QT's currentContext functions produce a generic set of functions that should be compatible with
     // many different versions of openGL independent of the users graphics card (within reason)
     QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
-    f->glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    f->initializeOpenGLFunctions();
 
-    // Initialize terrain, lights, and camera
+    // Enable depth test
+    f->glEnable(GL_DEPTH_TEST);
+    f->glDepthFunc(GL_LESS);
+
+    // Set sky color
+    f->glClearColor(SKY);
+
+    // Initialize terrain's OpenGL
     this->_terrain->initializeGL();
 }
 
@@ -52,18 +81,98 @@ void OpenGL::resizeGL(int w, int h)
 }
 
 // Paint the scene, lights, objects, etc.
-// TODO: add light
 void OpenGL::paintGL()
 {
     QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
 
     // Clear screen
-    f->glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    f->glClear(GL_COLOR_BUFFER_BIT);
+    f->glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Set sky color
+    f->glClearColor(SKY);
+
+    // Get camera details
+    QMatrix4x4 camera_matrix = this->_camera->matrix();
+    QVector3D camera_pos = this->_camera->position();
+
+    // Get light details
+    QVector3D light_color = this->_light->color();
+    QVector3D light_pos = this->_light->position();
+    float light_intensity = this->_light->intensity();
 
     // Draw terrain
-    // TODO: Include light parameter
-    this->_terrain->paintGL(f, this->_camera->matrix());
+    this->_terrain->paintGL(
+        f,
+        camera_matrix,
+        camera_pos,
+        light_color,
+        light_pos,
+        light_intensity);
+
+    // Draw the sun graphic
+    this->_light->paintGL(camera_matrix);
+
+    // Draw additional debug info (axis)
+    // TODO: Draw axis as an info graphic in the corner
+
+    float length = 2.0f;
+
+    // Get the axis vectors for drawing the world space zero axis compensated for the camera
+    QVector3D x = camera_matrix * QVector3D(length, 0.0f, 0.0f);   // (1,  0,  0)
+    QVector3D y = camera_matrix * QVector3D(0.0f, length, 0.0f);   // (0,  1,  0)
+    QVector3D z = camera_matrix * QVector3D(0.0f, 0.0f, length);   // (0,  0,  1)
+    QVector3D xn = camera_matrix * QVector3D(-length, 0.0f, 0.0f); // (-1, 0,  0)
+    QVector3D yn = camera_matrix * QVector3D(0.0f, -length, 0.0f); // (0, -1,  0)
+    QVector3D zn = camera_matrix * QVector3D(0.0f, 0.0f, -length); // (0,  0, -1)
+    QVector3D zero = camera_matrix * QVector3D(0.0f, 0.0f, 0.0f);  // (0,  0,  0)
+
+    // Draw the origin point (black dot at world (0, 0, 0))
+    glBegin(GL_POINTS);
+    glColor3f(0.0f, 0.0f, 0.0f);
+    glVertex3f(zero.x(), zero.y(), zero.z());
+    glEnd();
+
+    // Draw positive x-axis
+    glBegin(GL_LINES);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(x.x(), x.y(), x.z());
+    glVertex3f(zero.x(), zero.y(), zero.z());
+
+    // Draw positive y-axis
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(y.x(), y.y(), y.z());
+    glVertex3f(zero.x(), zero.y(), zero.z());
+
+    // Draw positive z-axis
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(z.x(), z.y(), z.z());
+    glVertex3f(zero.x(), zero.y(), zero.z());
+    glEnd();
+
+    // Draw dashed lines
+    glPushAttrib(GL_ENABLE_BIT);
+
+    glLineStipple(4, 0xAAAA);
+    glEnable(GL_LINE_STIPPLE);
+
+    // Draw negative x-axis
+    glBegin(GL_LINES);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(xn.x(), xn.y(), xn.z());
+    glVertex3f(zero.x(), zero.y(), zero.z());
+
+    // Draw negative y-axis
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(yn.x(), yn.y(), yn.z());
+    glVertex3f(zero.x(), zero.y(), zero.z());
+
+    // Draw negative z-axis
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(zn.x(), zn.y(), zn.z());
+    glVertex3f(zero.x(), zero.y(), zero.z());
+    glEnd();
+
+    glPopAttrib();
 }
 
 // On mouse wheel activity, zoom in (was implemented early to debug OpenGL implementation)
@@ -74,4 +183,36 @@ void OpenGL::wheelEvent(QWheelEvent *event)
     else
         this->_camera->zoom(-0.25f);
     this->update();
+}
+
+void OpenGL::mousePressEvent(QMouseEvent *event)
+{
+    this->_prev = event->pos();
+    this->_dragging = true;
+}
+
+void OpenGL::mouseReleaseEvent(QMouseEvent *event)
+{
+    (void *)event;
+    this->_dragging = false;
+}
+
+void OpenGL::mouseMoveEvent(QMouseEvent *event)
+{
+    Qt::KeyboardModifiers modifiers = event->modifiers();
+
+    QPoint delta = this->_prev - event->pos();
+    this->_prev = event->pos();
+
+    if (modifiers.testFlag(Qt::ShiftModifier))
+    {
+        this->_light->rotateX(delta.y());
+        this->_light->rotateY(delta.x());
+    }
+    else
+    {
+        this->_camera->rotateX(-delta.y());
+        this->_camera->rotateY(-delta.x());
+    }
+    update();
 }
--- src/OpenGL/opengl.h
+++ src/OpenGL/opengl.h
@@ -4,19 +4,20 @@
 #include <QOpenGLWidget>
 #include <QOpenGLFunctions>
 #include <QWheelEvent>
+#include <QMouseEvent>
+#include <QPoint>
 
 #include "./Terrain/terrain.h"
 #include "./Camera/camera.h"
 #include "./Light/light.h"
 
-// Custom OPenGL widget that manages and renders the OpenGL context to the window
+// Custom OpenGL widget that manages and renders the OpenGL context to the widget
 class OpenGL : public QOpenGLWidget
 {
 public:
     explicit OpenGL(QWidget *parent = 0);
     ~OpenGL();
 
-    // TODO: Add event handlers for pan/zoom camera and pan/zoom light
 protected:
     // Initialize gl functions and settings
     void initializeGL() override;
@@ -28,8 +29,22 @@ protected:
     // On mouse wheel scroll, zoom in/out
     void wheelEvent(QWheelEvent *event) override;
 
+    // Mouse click and draw rotate camera, shift to rotate light
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+
 private:
+    // Terrain handler
     Terrain *_terrain;
+    // Camera handler
     Camera *_camera;
+    // Light handler
     Light *_light;
+
+    // Switch to update data when mouse move is click drag
+    bool _dragging = false;
+
+    // Previous position for createing relative mouse movement data in mouseMoveEvent
+    QPoint _prev;
 };
