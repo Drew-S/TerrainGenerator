e3414bf7bcf36605676faa526774088de49c56f1
Drew Sommer <drwsommer@gmail.com>
2020-04-23 15:21:17 -0600

    Moved the nodeeditor to its own file

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "ui_Main.h"
 
 #include "Nodeeditor/nodeeditor.h"
+#include "OpenGL/opengl.h"
 
 #include <QApplication>
 #include <QMainWindow>
 #include <QDebug>
+#include <QObject>
 
 int main(int argc, char *argv[])
 {
@@ -17,7 +19,15 @@ int main(int argc, char *argv[])
     // Setup the main window
     ui.setupUi(&main_window);
 
+    // Manager for the Nodes flow and scene
     Nodeeditor editor(ui.NodeEditorLayout);
+
+    // Connect to the nodeeditor output update signal
+    QObject::connect(
+        &editor,
+        &Nodeeditor::outputUpdated,
+        ui.OpenGLWidget,
+        &OpenGL::nodeeditorOutputUpdated);
 
     // Fix splitter size with the nodeeditor
     ui.splitter_top_bottom->setSizes({300, 150});
