744119562d5a4ae756dd44745fd8957ee1f6f87f
Drew Sommer <drwsommer@gmail.com>
2020-04-28 13:47:22 -0600

    Converted from QPixmap to high detail double map

--- src/Nodeeditor/Datatypes/pixmap.h
+++ src/Nodeeditor/Datatypes/pixmap.h
@@ -4,20 +4,26 @@
 
 #include <lib/nodeeditor/include/nodes/NodeDataModel>
 
+#include "structures.h"
+
 // Manages the sharing of a pixmap between nodes
 // Used to send once image from one node to another
 class PixmapData : public QtNodes::NodeData
 {
 public:
     PixmapData(){};
-    PixmapData(QPixmap const &pixmap) : _pixmap(pixmap){};
+    PixmapData(QPixmap const &pixmap)
+    {
+        this->_vector_map = VectorMap(pixmap);
+    };
+    PixmapData(VectorMap const &vector_map) : _vector_map(vector_map){};
 
     // The type of data this is { QString: id, QString name}, name shows up on node
-    QtNodes::NodeDataType type() const override { return {"pixmap", "sampler2D"}; };
+    QtNodes::NodeDataType type() const override { return {"PixmapData", "sampler2D"}; };
 
-    // Returns the stored pixmap
-    QPixmap pixmap() const { return this->_pixmap; };
+    // Returns the stored colorMap
+    VectorMap vectorMap() const { return this->_vector_map; };
 
 private:
-    QPixmap _pixmap;
+    VectorMap _vector_map;
 };
