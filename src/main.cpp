d68f523532301dc09afe007445eaa1d859a23b2b
Drew Sommer <drwsommer@gmail.com>
2020-05-03 12:18:18 -0600

    Simplified main file

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,18 @@
 
 #include "mainwindow.h"
 
+#include "Nodeeditor/nodeeditor.h"
+
 #include <QApplication>
-#include <QMainWindow>
-#include <QDebug>
-#include <QObject>
 
 int main(int argc, char *argv[])
 {
     qDebug() << "Starting Program";
     QApplication app(argc, argv);
 
+    // Should colorize the connections between nodes based on their data type
+    // generated from the name of the data type (to fully customize I would
+    // need to change nodeeditor itself, which I may yet do)
     QtNodes::ConnectionStyle::setConnectionStyle(
         R"(
             "ConnectionStyle": {
@@ -21,6 +23,7 @@ int main(int argc, char *argv[])
             }
         )");
 
+    // Load ui for main window and attach it to custom QMainWindow
     Ui::MainWindow ui;
     MainWindow main_window;
 
