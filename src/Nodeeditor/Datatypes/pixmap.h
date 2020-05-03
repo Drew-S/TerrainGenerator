9af801caa1673ed27205bb13ee0fb170aa74afec
Drew Sommer <drwsommer@gmail.com>
2020-05-03 12:45:17 -0600

    Updated connector management for separate types

--- src/Nodeeditor/Datatypes/pixmap.h
+++ src/Nodeeditor/Datatypes/pixmap.h
@@ -4,22 +4,43 @@
 
 #include <lib/nodeeditor/include/nodes/NodeDataModel>
 
-#include "structures.h"
+#include "vectormap.h"
+#include "intensitymap.h"
 
-// Manages the sharing of a pixmap between nodes
-// Used to send once image from one node to another
-class PixmapData : public QtNodes::NodeData
+// Manages data transfer between nodes, manages Intensity maps
+class IntensityMapData : public QtNodes::NodeData
 {
 public:
-    PixmapData(){};
-    PixmapData(QPixmap const &pixmap)
+    IntensityMapData(){};
+    IntensityMapData(QPixmap const &pixmap)
+    {
+        this->_intensity_map = IntensityMap(pixmap, IntensityMap::AVERAGE);
+    };
+    IntensityMapData(IntensityMap const &intensity_map) : _intensity_map(intensity_map){};
+
+    // The type of data this is { QString: id, QString name}, name shows up on node
+    QtNodes::NodeDataType type() const override { return {"IntensityMap", "sampler2D"}; };
+
+    // Returns the stored colorMap
+    IntensityMap intensityMap() const { return this->_intensity_map; };
+
+private:
+    IntensityMap _intensity_map;
+};
+
+// Manages data transfer between nodes, manages Vector maps
+class VectorMapData : public QtNodes::NodeData
+{
+public:
+    VectorMapData(){};
+    VectorMapData(QPixmap const &pixmap)
     {
         this->_vector_map = VectorMap(pixmap);
     };
-    PixmapData(VectorMap const &vector_map) : _vector_map(vector_map){};
+    VectorMapData(VectorMap const &vector_map) : _vector_map(vector_map){};
 
     // The type of data this is { QString: id, QString name}, name shows up on node
-    QtNodes::NodeDataType type() const override { return {"PixmapData", "sampler2D"}; };
+    QtNodes::NodeDataType type() const override { return {"VectorMap", "sampler2D"}; };
 
     // Returns the stored colorMap
     VectorMap vectorMap() const { return this->_vector_map; };
