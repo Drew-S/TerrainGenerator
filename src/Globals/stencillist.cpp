88250b5a4ffb3a89285e18fdec9de7fee633c388
Drew Sommer <drwsommer@gmail.com>
2020-05-13 11:41:08 -0600

    Global list of stencils for drawing

--- src/Globals/stencillist.cpp
+++ src/Globals/stencillist.cpp
@@ -10,11 +10,14 @@
 
 #include <glm/vec4.hpp>
 
+// Create a new stencil
 Stencil::Stencil(QString filename, int index)
 {
     QPixmap pixmap(filename);
     this->_filename = filename;
     this->_stencil = IntensityMap(pixmap, IntensityMap::AVERAGE);
+
+    // Generate icon of the stencil
     this->_icon = QIcon(VectorMap::fromIntensityMap(this->_stencil,
                                                     glm::dvec4(1.00, 1.00, 1.00, 1.00),
                                                     VectorMap::OVERRIDE_COLOR)
@@ -23,7 +26,6 @@ Stencil::Stencil(QString filename, int index)
     this->_index = index;
     qDebug("Stencil '%s' (%d) loaded", qPrintable(this->name()), index);
 }
-
 Stencil::Stencil(QString filename, double brush, int index)
 {
     QPixmap pixmap(filename);
@@ -37,7 +39,6 @@ Stencil::Stencil(QString filename, double brush, int index)
     this->_generate();
     this->_index = index;
 }
-
 Stencil::Stencil(QString filename, double brush, QColor color, int index)
 {
     QPixmap pixmap(filename);
@@ -52,26 +53,31 @@ Stencil::Stencil(QString filename, double brush, QColor color, int index)
     this->_generate();
     this->_index = index;
 }
-
 Stencil::~Stencil() {}
 
+// Set the stencil color, ignores the alpha
 void Stencil::setColor(QColor color)
 {
-    this->_color = color;
+    this->_color.setRed(color.red());
+    this->_color.setGreen(color.green());
+    this->_color.setBlue(color.blue());
     this->_generate();
 }
 
+// Set the brush radius
 void Stencil::setBrush(double brush)
 {
     this->_brush = brush;
     this->_generate();
 }
 
+// Set the stencils opacity
 void Stencil::setAlpha(double value)
 {
     this->_color.setAlphaF(value);
 }
 
+// Draw onto the given painter with the position
 void Stencil::draw(QPainter *painter, QPointF pos)
 {
     painter->drawPixmap(
@@ -82,25 +88,30 @@ void Stencil::draw(QPainter *painter, QPointF pos)
         this->_brush, this->_brush);
 }
 
+// Get the index of the stencil in the stencil list
 int Stencil::index()
 {
     return this->_index;
 }
 
+// Get the name of the brush
 QString Stencil::name()
 {
     return QFileInfo(this->_filename)
         .fileName()
+        // TODO: remove brush-prefix
         .replace(QRegExp("^brush(-*)", Qt::CaseInsensitive), "")
         .replace(QRegExp("\\.(jpg|png|jpeg)$", Qt::CaseInsensitive), "")
         .replace(QRegExp("-"), " ");
 }
 
+// Get the icon representation of the stencil
 QIcon Stencil::icon()
 {
     return this->_icon;
 }
 
+// Generate the drawing pixmap used to stamp with the QPainter
 void Stencil::_generate()
 {
     glm::dvec4 color(this->_color.redF(), this->_color.greenF(), this->_color.blueF(), this->_color.alphaF());
@@ -109,11 +120,15 @@ void Stencil::_generate()
                         .scaled(this->_brush, this->_brush, Qt::KeepAspectRatio);
 }
 
+
+// StencilList singleton setup
 bool StencilList::_instance = false;
 StencilList *StencilList::_single = nullptr;
 
 StencilList::StencilList()
 {
+    // Get a list of stencils and load them into the program
+    // TODO: Use absolute path system
     QDir stencils("assets/stencils");
     QStringList images = stencils.entryList(QStringList() << "*.jpg"
                                                           << "*.png"
@@ -126,12 +141,12 @@ StencilList::StencilList()
         this->_stencils.push_back(Stencil(QString("assets/stencils/") + images.at(i), i));
     }
 }
-
 StencilList::~StencilList()
 {
     _instance = false;
 }
 
+// Get a reference to a specific stencil
 Stencil *StencilList::stencil(int index)
 {
     if (index < 0 || index >= this->count())
@@ -140,11 +155,13 @@ Stencil *StencilList::stencil(int index)
     return &this->_stencils[index];
 }
 
+// Get the number of stencils in the program
 int StencilList::count()
 {
     return (int)this->_stencils.size();
 }
 
+// Get/create a reference to the StencilList singleton
 StencilList *StencilList::getInstance()
 {
     if (!_instance)
--- src/Globals/stencillist.h
+++ src/Globals/stencillist.h
@@ -11,45 +11,88 @@
 
 #include "../Nodeeditor/Datatypes/intensitymap.h"
 
+// Stencil class loads a monocolour texture for drawing onto pixmaps
 class Stencil
 {
 public:
+    // Load from file. Index is reference to location in StencilList below
     Stencil(QString filename, int index);
+    // Set the brush size as well
     Stencil(QString filename, double brush, int index);
+    // Set the color of the stencil as well
     Stencil(QString filename, double brush, QColor color, int index);
     ~Stencil();
 
+    // Set the color of the stencil
     void setColor(QColor color);
+
+    // Set the opacity of the stencil color
     void setAlpha(double value);
+
+    // Set the brush radius
     void setBrush(double brush);
+
+    // Draw with the painter with position (painter draws to a
+    // specific pixmap in its reference)
     void draw(QPainter *painter, QPointF pos);
 
+    // Get the index of this stencil in the stencil list
     int index();
+
+    // Get the name of the stencil "brush-soft.png" -> "soft"
     QString name();
+
+    // Get an icon representation of this stencil
     QIcon icon();
 
 private:
-    int _index;
-    QString _filename;
+    // Generates pixmap used for the stencil from values
     void _generate();
+
+    // Location in stencil list
+    int _index;
+
+    // Filename the stencil is from
+    QString _filename;
+
+    // Radius of the brush
     double _brush = 8.00;
+
+    // Color of the brush
     QColor _color{0, 0, 0, 128};
+
+    // Intensity map version of the brush, used for masking
     IntensityMap _stencil;
+
+    // Pixmap representation used for drawing the onto a pixmap
     QPixmap _pixmap;
+
+    // Saved reference of the icon
     QIcon _icon;
 };
 
+// Global singleton that keeps a single reference for each stencil
 class StencilList
 {
 public:
+    // Get/create a reference to the singleton of stencils
     static StencilList *getInstance();
     ~StencilList();
+
+    // Get a specific stencil with the index
     Stencil *stencil(int index);
+
+    // Get the number of stencils loaded into the program
     int count();
 
 private:
+    // Singleton creation values
     StencilList();
     static bool _instance;
     static StencilList *_single;
+
+    // List of the stencils loaded
     std::vector<Stencil> _stencils;
 };
+
+#define STENCILS StencilList::getInstance()
