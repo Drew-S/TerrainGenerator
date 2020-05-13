70fe03ad2600c6ee9d342e397160512f3057a6d8
Drew Sommer <drwsommer@gmail.com>
2020-05-13 11:40:55 -0600

    Global list of textures for nodes and drawing

---
+++ src/Globals/texturelist.cpp
@@ -0,0 +1,132 @@
+#include "texturelist.h"
+
+#include <QBrush>
+#include <QColor>
+#include <QRect>
+
+// Constructors
+Texture::Texture() {}
+Texture::Texture(QString filename)
+{
+    this->_filename = filename;
+    this->_pixmap = QPixmap(filename);
+    this->_painter = new QPainter(&this->_pixmap);
+}
+Texture::Texture(int w, int h)
+{
+    this->_pixmap = QPixmap(w, h);
+    this->_painter = new QPainter(&this->_pixmap);
+    this->_painter->setBrush(QBrush(QColor(255, 255, 255, 255)));
+    this->_painter->drawRect(QRect(0, 0, w, h));
+}
+Texture::Texture(int w, int h, QString filename)
+{
+    this->_filename = filename;
+    this->_pixmap = QPixmap(w, h);
+    this->_painter = new QPainter(&this->_pixmap);
+    this->_painter->setBrush(QBrush(QColor(255, 255, 255, 255)));
+    this->_painter->drawRect(QRect(0, 0, w, h));
+}
+Texture::~Texture() {}
+
+// Return a pixmap
+QPixmap Texture::pixmap() { return this->_pixmap; }
+
+// Return an image
+QImage Texture::image() { return this->_pixmap.toImage(); }
+
+// Return an icon
+QIcon Texture::icon() { return QIcon(this->_pixmap); }
+
+// Return a vector map
+VectorMap Texture::vectorMap() { return VectorMap(this->_pixmap); }
+
+// Return an intensity map
+IntensityMap Texture::intensityMap(IntensityMap::Channel channel) { return IntensityMap(this->_pixmap, channel); }
+
+// Draw the stencil onto the pixmap
+void Texture::draw(Stencil *stencil, QPointF pos, bool update)
+{
+    stencil->draw(this->_painter, pos);
+    if (update)
+        emit this->updated();
+}
+
+// Emit updated signal
+void Texture::apply()
+{
+    emit this->updated();
+}
+
+// Save to the existing file
+bool Texture::save()
+{
+    if (this->_filename == "")
+        return false;
+    return this->saveAs(this->_filename);
+}
+
+// Save to a specific file
+bool Texture::saveAs(QString filename)
+{
+    return this->_pixmap.save(filename, 0, 100);
+}
+
+// Singleton setup
+bool TextureList::_instance = false;
+TextureList *TextureList::_single = nullptr;
+int TextureList::_num = 1;
+
+TextureList::TextureList() {}
+
+TextureList::~TextureList()
+{
+    _instance = false;
+}
+
+// Get/create refernce to the singleton
+TextureList *TextureList::getInstance()
+{
+    if (!_instance)
+    {
+        _instance = true;
+        _single = new TextureList();
+    }
+    return _single;
+}
+
+// Add a texture with the given file, returns the index for at() use
+// TODO: Add duplicate file check
+// TODO: Add file exists check
+int TextureList::addTexture(QString filename)
+{
+    Texture *t = new Texture(filename);
+    this->_textures.push_back(t);
+    emit this->textureAdded((int)this->_textures.size() - 1);
+    return (int)this->_textures.size() - 1;
+}
+
+// Add a texture with the given size, returns the index for at() use
+int TextureList::addTexture(int w, int h)
+{
+    Texture *t = new Texture(w, h, QString("Texture_") + QString::number(_num));
+    _num++;
+    this->_textures.push_back(t);
+    emit this->textureAdded((int)this->_textures.size() - 1);
+    return (int)this->_textures.size() - 1;
+}
+
+// Get the number of textures listed
+int TextureList::count()
+{
+    return (int)this->_textures.size() - 1;
+}
+
+// Get the refernce for a specific texture,
+// returns nullptr if the index beyond bounds
+Texture *TextureList::at(int index)
+{
+    if (index < 0 || index >= (int)this->_textures.size())
+        return nullptr;
+    return this->_textures[index];
+}
---
+++ src/Globals/texturelist.h
@@ -0,0 +1,111 @@
+#pragma once
+
+#include <vector>
+
+#include <QObject>
+#include <QPixmap>
+#include <QImage>
+#include <QIcon>
+#include <QPainter>
+#include <QPointF>
+
+#include "Globals/stencillist.h"
+#include "../Nodeeditor/Datatypes/vectormap.h"
+#include "../Nodeeditor/Datatypes/intensitymap.h"
+
+// Texture class houses pixmap that can be drawn on,
+// used to share textures with pointer (multiple texture
+// nodes or the drawing dialogue)
+class Texture : public QObject
+{
+    Q_OBJECT
+public:
+    // Create textures
+    Texture();
+    // From file
+    Texture(QString filename);
+    // With size and default colour (white)
+    Texture(int w, int h);
+    // With size and a filename
+    Texture(int w, int h, QString filename);
+    ~Texture();
+
+    // Convert to pixmap
+    QPixmap pixmap();
+
+    // Convert to image
+    QImage image();
+
+    // Convert to icon
+    QIcon icon();
+
+    // Convert to vector map
+    VectorMap vectorMap();
+
+    // Convert to intensity map
+    IntensityMap intensityMap(IntensityMap::Channel channel);
+
+    // Draw on the texture with the stencil at a specific point,
+    // if update emit updated() signal
+    void draw(Stencil *stencil, QPointF pos, bool update = false);
+
+    // Call updated signal
+    void apply();
+
+    // Save the texture with the existing filename
+    bool save();
+
+    // Overright save location
+    bool saveAs(QString filename);
+
+signals:
+    // Called after drawing if update true
+    void updated();
+
+private:
+    QString _filename = "";
+    QPixmap _pixmap;
+    // Painter for drawing on the _pixmap
+    QPainter *_painter;
+};
+
+// Global singleton that manages all the Texture's provided in the system
+// This is used to allow nodes to select existing loaded textures from
+// as central location rather than separate management. Makes draw management
+// easier and allows for multiple nodes to reference the same texture without
+// duplicating data
+class TextureList : public QObject
+{
+    Q_OBJECT
+public:
+    // Get/create reference to TextureList singleton
+    static TextureList *getInstance();
+    ~TextureList();
+
+    // Add a texture to the list (from file)
+    int addTexture(QString filename);
+
+    // Add a new texture to the list
+    int addTexture(int w, int h);
+
+    // Get the number of textures
+    int count();
+
+    // Get the reference to a specific texture,
+    // returns nullptr if beyond bounds
+    Texture *at(int index);
+
+signals:
+    // Called when a new texture is added
+    void textureAdded(int index);
+
+private:
+    TextureList();
+    // The list of referenced textures
+    std::vector<Texture *> _textures;
+    static bool _instance;
+    static TextureList *_single;
+    static int _num;
+};
+
+#define TEXTURES TextureList::getInstance()
