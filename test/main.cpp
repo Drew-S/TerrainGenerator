66f9786b3abc029c3d4ddfd932293212f2967a39
Drew Sommer <drwsommer@gmail.com>
2020-05-18 13:24:21 -0600

    Added many more tests

--- test/main.cpp
+++ test/main.cpp
@@ -2,8 +2,13 @@
 
 #include <QApplication>
 
-#include "intensitymap_test.h"
-#include "vectormap_test.h"
+#include "./tests/intensitymap_test.h"
+#include "./tests/vectormap_test.h"
+#include "./tests/pixmap_test.h"
+#include "./tests/converters_test.h"
+#include "./tests/normal_test.h"
+#include "./tests/inputsimplexnoise_test.h"
+#include "./tests/inputtexture_test.h"
 
 int main(int argc, char *argv[])
 {
@@ -18,6 +23,12 @@ int main(int argc, char *argv[])
 
     ASSERT_TEST(new IntensityMap_Test());
     ASSERT_TEST(new VectorMap_Test());
+    ASSERT_TEST(new IntensityMapData_Test());
+    ASSERT_TEST(new IntensityToVectorMapConverter_Test());
+    ASSERT_TEST(new VectorToIntensityMapConverter_Test());
+    ASSERT_TEST(new NormalMapGenerator_Test());
+    ASSERT_TEST(new InputSimplexNoiseNode_Test());
+    ASSERT_TEST(new InputTextureNode_Test());
 
     return 0;
 }
