de8f17156339bc42f5ec462e45ff2138770f17fd
Drew Sommer <drwsommer@gmail.com>
2020-05-19 11:54:19 -0600

    Improved testing wrapper

--- test/test_wrapper.sh
+++ test/test_wrapper.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
+NOPASS=true
+
+if [ "$1" == "-p" ]; then
+    NOPASS=false
+fi
+
 pushd .. &>/dev/null
 
 RESULTS="$(./test/build/test)"
 
-popd
+popd &>/dev/null
+
+clear
 
 FAILS=$(echo "$RESULTS" | grep FAIL | wc -l)
 PASSES=$(echo "$RESULTS" | grep PASS | wc -l)
 TOTALS=$((FAILS + PASSES))
-PERC=$(bc <<< "scale=2; $PASSES/$TOTALS*100")
+PERC=$(bc <<< "scale=4; $PASSES/$TOTALS*100")
 
-echo "$RESULTS" | GREP_COLOR='31' grep --color=always -A 1 'FAIL'
+if test $NOPASS; then
+    echo "$RESULTS" | GREP_COLOR='31' egrep --color=always 'FAIL|$' | GREP_COLOR='33' egrep --color=always 'QWARN|$' | egrep 'QWARN|FAIL|Loc'
+else
+    echo "$RESULTS" | GREP_COLOR='31' egrep --color=always 'FAIL|$' | GREP_COLOR='33' egrep --color=always 'QWARN|$' | GREP_COLOR='32' egrep --color=always 'PASS|$' | egrep 'PASS|QWARN|FAIL|Loc'
+fi
+
+printf "\e[31m$FAILS\e[m Test(s) failed, \e[32m$PASSES\e[m Test(s) passed. "
 
 if (( $(echo "$PERC > 70" | bc -l ) )); then
-    printf "\e[31m$FAILS\e[m Test(s) failed, \e[32m$PASSES\e[m Test(s) passed. \e[32m$PASSES/$TOTALS\e[m, \e[32m%0.2f%%\e[m\n" $PERC
+    printf "\e[32m$PASSES\e[m/\e[32m$TOTALS\e[m, \e[32m%0.2f%%\e[m\n" $PERC
 
 elif (( $(echo "$PERC > 50" | bc -l ) )); then
-    printf "\e[31m$FAILS\e[m Test(s) failed, \e[32m$PASSES\e[m Test(s) passed. \e[33m$PASSES/$TOTALS\e[m, \e[33m%0.2f%%\e[m\n" $PERC
+    printf "\e[33m$PASSES\e[m/\e[32m$TOTALS\e[m, \e[33m%0.2f%%\e[m\n" $PERC
 
 else
-    printf "\e[31m$FAILS\e[m Test(s) failed, \e[32m$PASSES\e[m Test(s) passed. \e[31m$PASSES/$TOTALS\e[m, \e[31m%0.2f%%\e[m\n" $PERC
+    printf "\e[31m$PASSES\e[m/\e[32m$TOTALS\e[m, \e[31m%0.2f%%\e[m\n" $PERC
 
 fi
